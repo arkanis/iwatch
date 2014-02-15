e744acd2c9e91d97142bf03443327964e06b3d47
Stephan <stephan.soller@helionweb.de>
2014-02-15 03:52:07 +0100

    Initial version, basic functionality.

---
+++ Makefile
@@ -0,0 +1,5 @@
+iwatch:
+	gcc iwatch.c -o iwatch
+
+clean:
+	rm -f iwatch
---
+++ iwatch.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/inotify.h>
+#include <sys/ioctl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <sys/wait.h>
+#include <sys/types.h>
+
+int main(int argc, char **argv){
+	
+	if (argc < 4 || strcmp(argv[2], "--") != 0){
+		fprintf(stderr, "usage: %s target-file -- command\n", argv[0]);
+		return -1;
+	}
+	
+	char *target_file = argv[1];
+	char *action = argv[3];
+	
+	int event_queue = inotify_init();
+	if (event_queue == -1){
+		perror("inotify_init");
+		return -1;
+	}
+	
+	int watch_id = inotify_add_watch(event_queue, target_file, IN_CREATE | IN_CLOSE_WRITE);
+	if (watch_id == -1){
+		perror("inotify_add_watch");
+		return -1;
+	}
+	
+	char buffer[4096];
+	struct inotify_event *event = (void*)buffer;
+	while(true){
+		int bytes_read = read(event_queue, event, sizeof(buffer));
+		if (bytes_read == -1){
+			perror("read of event structure failed");
+			break;
+		}
+		
+		void exec_command(){
+			pid_t child = fork();
+			if (child == 0) {
+				execl("/bin/sh", "/bin/sh", "-c", action, (char*)NULL);
+			} else {
+				waitpid(child, NULL, 0);
+			}
+		}
+		
+		if (event->mask & IN_CREATE){
+			printf("file created in %s, executing: %s\n", target_file, action);
+			exec_command();
+		}
+		if (event->mask & IN_CLOSE_WRITE){
+			printf("file written to, executing: %s\n", action);
+			exec_command();
+		}
+		
+		if (event->mask & IN_DELETE_SELF) {
+			printf("%s deleted, nothing more to watch. Have a nice day.\n", target_file);
+			break;
+		}
+	}
+	
+	close(event_queue);
+	
+	return 0;
+}
